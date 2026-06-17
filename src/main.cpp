55a6d366e74daa903a8420219166ff6ae499777a
Cocoa Milka <chocolatemilkcows@gmail.com>
2026-06-17 21:04:47 +0200

    holy drawing triangle now

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+
+#define GL_SILENCE_DEPRECATION
+#define GLFW_INCLUDE_GLCOREARB
 #include <GLFW/glfw3.h>
 
 #include "input.hpp"
+
+const char *vertexShaderSource = 
+	"#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource =
+	"#version 330 core\n"
+	"out vec4 FragColor;\n"
+	"void main()\n"
+	"{\n"
+	"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"}\n";
 
 int main()
 {
@@ -12,6 +31,15 @@ int main()
 	// init library
 	if (!glfwInit())
 		return -1;
+
+	// mac is a bitch, have to use special versions
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	#endif
 
 	// create window with opengl context
 	window = glfwCreateWindow(640, 480, "Leaf Skitter", NULL, NULL);
@@ -27,10 +55,70 @@ int main()
 	// reference input.hpp for keycallbacks
 	glfwSetKeyCallback(window, key_callback);
 
+	// compiling shader sob we are storing as string, fix later
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	// make sure this shit compiled properly
+	int success;
+	char infoLog[512];
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "fragment shader compilation failed!!\n" << infoLog << std::endl;
+	}
+
+	// creating shader program
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	// link program with compiled shaders
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	
+	// no longer needed
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	
+	// rendering shit rm this shit later
+	float vertices[] =
+	{
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f
+	};
+
+	// create buffers, omg just like the simulations! :3
+	unsigned int VBO, VAO;
+
+	glGenBuffers(1, &VBO);
+	glGenVertexArrays(1, &VAO);
+
+	glBindVertexArray(VAO);	
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	
+	// set vertex attrib pointers
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// use compiled shaders
+	glUseProgram(shaderProgram);
+
 	while(!glfwWindowShouldClose(window))
 	{
 		// render goes here!!
 		glClear(GL_COLOR_BUFFER_BIT);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		
 		// swap front and back buffers
 		glfwSwapBuffers(window);
